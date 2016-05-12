42937bf9e411fa9292baa0f8c72d9da8adce1b11
Pasiuta Vitaliy <Pasuta_V@ukr.net>
2016-05-12 22:21:09 +0300

    u

--- app.js
+++ app.js
@@ -26,6 +26,31 @@ app.use(route.get('/', require('./controllers/site/index')));
 app.use(route.get('/contacts', require('./controllers/site/contacts')));
 app.use(route.get('/about', require('./controllers/site/about')));
 
+app.use(function *pageNotFound(next){
+    yield next;
+
+    if (404 != this.status) return;
+
+    // we need to explicitly set 404 here
+    // so that koa doesn't assign 200 on body=
+    this.status = 404;
+
+    switch (this.accepts('html', 'json')) {
+        case 'html':
+            this.type = 'html';
+            this.body = yield render('404', { });
+            break;
+        case 'json':
+            this.body = {
+                message: 'Page Not Found2'
+            };
+            break;
+        default:
+            this.type = 'text';
+            this.body = 'Page Not Found3';
+    }
+});
+
 app.use(serve(__dirname + '/public'));
 
 app.listen(3001);
