b42b4794f11a142bc2d835b24411fd556968fc50
Pasiuta Vitaliy <Pasuta_V@ukr.net>
2016-05-17 22:20:48 +0300

    u

--- public/site/js/tm-scripts.js
+++ public/site/js/tm-scripts.js
@@ -416,58 +416,6 @@ var o = $('#camera');
 })(jQuery);
 
 
-
-
-
-/* Facebook
-========================================================*/
-
-;(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.3";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-;(function ($) {
-var o = $('.fb-page');
-
-$(window).load(function () {
-o.css({'display': 'block'})
-.find('span').css({
-'width': '100%',
-'display': 'block',
-'text-align': 'inherit'
-})
-.find('iframe').css({
-'display': 'inline-block',
-'position': 'static'
-});
-});
-
-$(window).on('load resize', function () {
-if (o.parent().width() < o.find('iframe').width()) {
-o.find('iframe').css({
-'transform': 'scale(' + (o.width() / o.find('iframe').width()) + ')',
-'transform-origin': '0% 0%'
-})
-.parent().css({
-'height': (o.find('iframe').height() * (o.width() / o.find('iframe').width())) + 'px'
-});
-} else {
-o
-.find('span').css({
-'height': 'auto'
-})
-.find('iframe').css({
-'transform': 'none'
-});
-}
-});
-})(jQuery);
-
-
 /* FancyBox
 ========================================================*/
 ;(function ($) {
