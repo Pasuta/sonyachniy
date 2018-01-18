b493473766bd72c0a89e1dd12082b16e13d898c0
Vitaliy Pasiuta <vpas@ciklum.com>
2018-01-18 11:05:44 +0200

    fix contacts

--- controllers/admin/media/uploadMedia.js
+++ controllers/admin/media/uploadMedia.js
@@ -1,56 +1,39 @@
 'use strict';
 
-var parse = require('co-busboy');
-var path = require('path');
-var fs = require('co-fs');
-var saveTo = require('save-to');
+const parse = require('co-busboy');
+const path = require('path');
+const fs = require('co-fs');
+const saveTo = require('save-to');
 const appDir = path.dirname(require.main.filename);
 
+module.exports = function *(name) {
+    let savedFile = '';
+    const uuid = uid();
+    const parts = parse(this, {
+        autoFields: true
+    });
 
-module.exports = function *() {
-    console.log('x');
-    // var parts = parse(this, {
-    //     autoFields: true // saves the fields to parts.field(s)
-    // });
-    //
-    // console.log(parts);
-    // parse the multipart body
-    // var savedFile = '';
-    // var uuid = uid();
-    // var parts = parse(this, {
-    //     autoFields: true // saves the fields to parts.field(s)
-    // });
-    //
-    // // create a temporary folder to store files
-    // var tmpdir = path.join(appDir, 'media', uid());
-    //
-    // // make the temporary directory
-    // yield fs.mkdir(tmpdir);
-    //
-    // console.log(1);
-    // // list of all the files
-    // var files = [];
-    // var file;
-    //
-    // // yield each part as a stream
-    // var part;
-    // console.log(2);
-    // console.log(parts);
-    // while (part = yield parts) {
-    //     // filename for this part
-    //     savedFile = uuid + '/' + part.filename;
-    //     console.log(savedFile);
-    //     files.push(file = path.join(tmpdir, part.filename));
-    //     // save the file
-    //     yield saveTo(part, file);
-    // }
-    //
-    // // return all the filenames as an array
-    // // after all the files have finished downloading
-    // // console.log(savedFile);
-    // this.body = yield {
-    //     "filepath": savedFile
-    // };
+    // list of all the files
+    const files = [];
+    let file;
+
+    // yield each part as a stream
+    let part;
+    while (part = yield parts) {
+        const tmpdir = path.join(appDir, 'media', uid());
+        yield fs.mkdir(tmpdir);
+        savedFile = uuid + '/' + part.filename;
+        files.push(file = path.join(tmpdir, part.filename));
+        yield saveTo(part, file);
+    }
+
+    console.log(files);
+
+    this.body = yield {
+        "status": "ok",
+        "code": 200,
+        "result": files
+    };
 };
 
 function uid() {
---
+++ public/admin/js/upload.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.date-picker').datepicker("update", new Date($("#date").val()));
+});
+
+var uploadPhoto = document.getElementById('uploadPhoto');
+Event.add(uploadPhoto, 'submit', function (e) {
+    e.preventDefault();
+
+    var data = new FormData();
+    jQuery.each(jQuery('#input')[0].files, function(i, file) {
+        data.append('file-'+i, file);
+    });
+
+    jQuery.ajax({
+        url: '/admin/media/uploadMedia',
+        data: data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        method: 'POST',
+        type: 'POST',
+        error: function(data){
+            console.log(data.responseJSON);
+        }
+    });
+});
