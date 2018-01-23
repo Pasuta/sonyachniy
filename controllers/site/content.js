f198292147832753d1a07de7885e0399d913a4b2
Vitaliy Pasiuta <vpas@ciklum.com>
2018-01-23 21:30:29 +0200

    Add form upload page

--- controllers/site/content.js
+++ controllers/site/content.js
@@ -1,15 +1,24 @@
-var render = require('../../lib/render');
+const render = require('../../lib/render');
+const path = require('path');
+const fs = require('fs');
 
 const Promise = require("bluebird");
 const Page = Promise.promisifyAll(require('../../models/page'));
 
+const appDir = path.dirname(require.main.filename);
+const filePath = path.join(appDir, 'public', 'download');
+
 module.exports = function *(page) {
     const doc = yield Promise.promisify(Page.findOne, {context: Page})({'page': page});
+    let file = '';
 
     if (!doc) {
         this.type = 'html';
         this.body = yield render.site('404', { });
     } else {
-        this.body = yield render.site('content', { doc: doc });
+        if (page === 'jobs') {
+            file = fs.readdirSync(filePath)[0];
+        }
+        this.body = yield render.site('content', { doc: doc, file: file });
     }
 };
